cb26f4a0266400861281bff0bb00b5c55237bb9d
Philip Daveby <philip.daveby@gmail.com>
2021-07-01 14:42:48 +0200

    possibility to add and remove an exercise from workout, without comments

--- hack-day/client/src/components/ExercisePage.js
+++ hack-day/client/src/components/ExercisePage.js
@@ -14,7 +14,6 @@ const ExercisePage = () => {
   }, []);
 
   useEffect(() => {
-    // exercises ? exercises.map(e => console.log(e.clicked)) : console.log();
     console.log(workouts);
   });
   
@@ -32,8 +31,6 @@ const ExercisePage = () => {
       setFilteredExercises(exercises);
     }
     const filteredTitle = exercises.filter(exercise => exercise.title.match(query));
-    // const filteredCategory = exercises.filter(exercise => exercise.category.match(query));
-    // const matches = [...filteredTitle, ...filteredCategory];
     setFilteredExercises(filteredTitle);
   };
 
@@ -42,9 +39,6 @@ const ExercisePage = () => {
       if (obj.id.toString() === e.currentTarget.id) {
         workouts.splice(i, 1);
         setWorkouts(workouts);
-        // console.log('Should be removed now');
-        // Ta bort övningen
-        // Isf vill jag inte gå vidare och push till array
       }
     });
   };
@@ -54,87 +48,18 @@ const ExercisePage = () => {
       if (e.currentTarget.id !== exercise.id.toString()) {
         return;
       }
-      // 
       const updatedWorkout = [...workouts, exercise]
       setWorkouts(updatedWorkout);
     });
   };
 
   const toggleWorkout = e => {
-    // kolla om övningen finns
-
     const objExists = workouts.filter(exercise => exercise.id.toString() === e.currentTarget.id);
-    // console.log(objExists[0]);
     if (objExists[0]) {
       removeWorkout(e);
       return;
     }
-
-    // if (/*  */)
-
     addWorkout(e);
-    
-  // Lägger till övningen
-  
-
-  //   const i = workouts.length - 1;
-  //   const updatedWorkout = workouts[i];
-
-  //   if (workouts[i].exercises.length === 0) {
-  //     console.log('Inside check for empty array')
-  //     updatedWorkout.exercises.push(exercise);
-  //     setWorkouts([...workouts, updatedWorkout]);
-  //     return;
-  //   } 
-
-  //   workouts[i].exercises.forEach(exerciseObj => {
-
-  //     // console.log('inside for each')
-  //     // console.log(exerciseObj.id, ' - ', exercise.id)
-  //     if (exerciseObj.id !== exercise.id) {
-  //       // console.log('inside inner if')
-  //       updatedWorkout.exercises.push(exercise);
-  //       setWorkouts([...workouts, updatedWorkout]);
-  //       return;
-  //     } 
-      
-  //     console.log('Last')
-  //     const index = workouts[i].exercises.findIndex(e => e.id === exerciseObj.id);
-  //     // console.log(index)
-
-  //     updatedWorkout.exercises.splice(index, 1);
-  //     // console.log(updatedWorkout.exercises.splice(index, 1), ' console');
-  //     // console.log(updatedWorkout, ' console updated')
-  //     setWorkouts([...workouts, updatedWorkout]);
-  //     else {
-  //     }
-  //   });
-    
-
-
-
-
-
-
-  //     workouts[i].exercises.map(workoutExercise => {
-  //       if (workoutExercise.id === exercise.id) {
-  //         // hitta index sen ta bort
-  //         const index = workouts.findIndex(e => e.id === workoutExercise.id);
-
-  //         console.log('Inside dubble if')
-  //         return;
-  //         // console.log(index)
-  //         // workouts[i].exercises.splice(index, 1);
-  //       } else {
-  //         // console.log(workouts[i].exercises)
-  //         updatedWorkout.exercises.push(exercise);
-  //       }
-  //       setWorkouts([...workouts, updatedWorkout]);
-
-  //     });
-  //     Kolla om workout finns
-  //     console.log(workouts[i].exercises)
-  
   };
 
   const toggleChooseExercise = e => {
@@ -145,7 +70,6 @@ const ExercisePage = () => {
       }
       return exercise;
     }));
-    // lägg till exercise i workout
     toggleWorkout(e);
   };
 
