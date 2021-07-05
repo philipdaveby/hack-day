044c1e0829c6792e527317709fd768575fb3a2ee
Philip Daveby <philip.daveby@gmail.com>
2021-07-05 23:00:52 +0200

    updated restart button to mark all as undone plus UI fix

--- hack-day/client/src/components/Workout.js
+++ hack-day/client/src/components/Workout.js
@@ -25,16 +25,33 @@ const Workout = props => {
     window.localStorage.setItem('workouts', JSON.stringify(props.workouts))
   };
 
+  const restart = e => {
+    e.stopPropagation();
+
+    props.setWorkouts(props.workouts.map(exercises => {
+      if (exercises.id.toString() === e.currentTarget.parentNode.parentNode.parentNode.parentNode.id) {
+        exercises.workout.map(exercise => {
+          exercise.done = false;
+          return exercise;
+        });
+      }
+      return exercises;
+    }));
+  }
+
   return (
     <div>
-      <ul className="workouts__list">
+      <ul className="workout__list">
         {props.workout[0] ? props.workout.map(obj => {
           return <li key={obj.id} id={obj.id} onClick={e => toggleDone(e)} className={obj.done ? "workouts__exercise done" : "workouts__exercise"}>
             <h2>{obj.title}</h2>
             <p>{obj.category}</p>
             </li>}) : ''
         }
-        <button id={props.workout.id} className="workouts__button-delete" onClick={e => deleteWorkout(e)}>DELETE</button>
+        <div className="workouts__buttons">
+          <button id={props.workout.id} className="workouts__button-delete" onClick={e => deleteWorkout(e)}>DELETE</button>
+          <button id={props.workout.id} className="workouts__button-restart" onClick={e => restart(e)}>RESTART</button>
+        </div>
       </ul>
     </div>
   );
--- hack-day/client/src/components/Workouts.js
+++ hack-day/client/src/components/Workouts.js
@@ -8,7 +8,7 @@ const Workouts = props => {
   return (
     <div>
       <h2>Your workouts</h2>
-      <ul>{props.workouts.map((workout, index) => {
+      <ul className="workouts-page__list">{props.workouts.map((workout, index) => {
         return <li key={workout.id} id={workout.id} onClick={e => setActiveWorkout( (activeWorkout === workout.id) ? null : workout.id)} className="workouts__li">
            <h2>{workout.title}</h2>
            {(activeWorkout === workout.id) ? <Workout workout={workout.workout} workouts={props.workouts} setWorkouts={props.setWorkouts} /> : ''}
