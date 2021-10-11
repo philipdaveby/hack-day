ff863120b16d1a3456fd33fb1969e108c78c0a19
philipdaveby <73835849+philipdaveby@users.noreply.github.com>
2021-10-11 16:59:54 +0200

    removed possibility to update email and added check for old password when updating password

--- hack-day/src/components/Profile.js
+++ hack-day/src/components/Profile.js
@@ -31,7 +31,7 @@ const Profile = () => {
                 </ul>
                 {error && <p className="profile__error">{error}</p>}
                 <div className="profile__buttons">
-                    <button className="profile__logout-button" onClick={() => history.push('/update-profile')}>Update Profile</button>
+                    <button className="profile__logout-button" onClick={() => history.push('/update-profile')}>Update password</button>
                     <button className="profile__logout-button" onClick={handleLogout}>Log out</button>
                 </div>
             </section>
--- hack-day/src/components/UpdateProfile.js
+++ hack-day/src/components/UpdateProfile.js
@@ -1,41 +1,65 @@
 import React, { useRef, useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const UpdateProfile = () => {
 
-    const emailRef = useRef(null);
-    const passwordRef = useRef(null);
+    // const emailRef = useRef(null);
+    const oldPasswordRef = useRef(null);
+    const newPasswordRef = useRef(null);
     const passwordConfirmationRef = useRef(null);
-    const { currentUser, updateEmail, updatePassword } = useAuth();
-    const [error, setError] = useState('');
+    const { currentUser, updatePassword, login } = useAuth();
     const [loading, setLoading] = useState(false);
     const history = useHistory();
+    const notify = text => toast(text);
 
 
     const handleSubmit = async e => {
         e.preventDefault();
         const promises = [];
-
-        if (passwordRef.current.value !== passwordConfirmationRef.current.value) {
-            return setError('Passwords do not match');
-        }
-        if (passwordRef.current.value) {
-            promises.push(updatePassword(passwordRef.current.value));
-        }
-
         setLoading(true);
-        setError('');
-        if (emailRef.current.value !== currentUser.email) {
-            promises.push(updateEmail(emailRef.current.value));
+
+        if (newPasswordRef.current.value !== passwordConfirmationRef.current.value) {
+            notify('Your new passwords does not match, please try again');
+            passwordConfirmationRef.current.value = '';
+            return oldPasswordRef.current.value = '';
         }
+        
+        try {
+            setLoading(true)
+            await login(currentUser.email, oldPasswordRef.current.value);
+
+        } catch {
+            notify('Your old password is not correct, please try again');
+            return oldPasswordRef.current.value = '';
+        }
+
+        try {
+            setLoading(true)
+            await promises.push(updatePassword(newPasswordRef.current.value));
+            notify('Your password has been updated')
+
+        } catch {
+            notify('Your password could not be updated, please try again');
+            newPasswordRef.current.value = '';
+            passwordConfirmationRef.current.value = '';
+        }
+
+        // if (emailRef.current.value !== currentUser.email) {
+        //     promises.push(updateEmail(emailRef.current.value));
+        //     notify('Your email has been updated');
+        // }
 
         Promise.all(promises)
             .then(() => {
-                history.push('/profile');
+                setTimeout(() => {
+                    history.push('/profile');
+                }, 1500);
             })
             .catch(() => {
-                setError('Failed to update account');
+                notify('Failed to update account, please try again');
             })
             .finally(() => {
                 setLoading(false);
@@ -44,9 +68,9 @@ const UpdateProfile = () => {
 
     return (
         <div className="update-profile">
-            <h2 className="h2">Update profile</h2>
-            {error && <h1 className="error-text">The error: {error}</h1>}
+            <h2 className="h2">Update your email or password</h2>
             <form className="update-profile__form" onSubmit={e => handleSubmit(e)}>
+            {/* <p>Email</p>
                 <input
                     className="update-profile__input"
                     type="email"
@@ -56,14 +80,22 @@ const UpdateProfile = () => {
                     placeholder="Email"
                     required
                     defaultValue={currentUser.email}
+                /> */}
+                <input
+                    className="update-profile__input"
+                    type="password"
+                    ref={oldPasswordRef}
+                    name="oldPassword"
+                    id="oldPassword"
+                    placeholder="Old password"
                 />
                 <input
                     className="update-profile__input"
                     type="password"
-                    ref={passwordRef}
-                    name="password"
-                    id="password"
-                    placeholder="Leave blank to keep the same"
+                    ref={newPasswordRef}
+                    name="newPassword"
+                    id="newPassword"
+                    placeholder="New password"
                 />
                 <input
                     className="update-profile__input"
@@ -71,13 +103,14 @@ const UpdateProfile = () => {
                     ref={passwordConfirmationRef}
                     name="password-confirmation"
                     id="password-confirmation"
-                    placeholder="Leave blank to keep the same"
+                    placeholder="Confirm password"
                 />
                 <div className="update-profile__buttons">
                 <button className="update-profile__button" disabled={loading} type="submit">Update</button>
                 <button className="update-profile__cancel" onClick={() => history.push('/')}>Cancel</button>
                 </div>
             </form>
+            <ToastContainer />
         </div>
     );
 }
